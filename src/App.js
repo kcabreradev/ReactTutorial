3e4971680e2773f0c6a1c708bc29860ec84e3da5
kcabrera <kcabrera@tiempodevelopment.com>
2021-02-19 09:59:26 -0700

    added api call using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
-  return (
-    <div className="App">
-        First change.
-    </div>
-  );
+import { Component } from "react";
+import axios from 'axios'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        axios('https://api.randomuser.me/?results=5')
+            .then(response => {
+                this.setState({
+                    users: response.data.results
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                First change.
+            </div>
+        );
+    }
 }
 
 export default App;
