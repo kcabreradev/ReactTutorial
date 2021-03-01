24d78652d0062df40a32dfae3df734b8651befdf
kcabrera <kcabrera@tiempodevelopment.com>
2021-03-01 11:48:41 -0700

    handle clicks to load more users

--- src/App.js
+++ src/App.js
@@ -5,24 +5,40 @@ import { Loading } from './Loading';
 class App extends Component {
     constructor(props) {
         super(props)
+        //state
         this.state = {
             users: [],
             loading: false
         }
+        //bind
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleMore = this.handleMore.bind(this)
     }
 
     componentDidMount() {
         this.getUsers();
     }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.getUsers();
+        console.log('different users loaded');
+    }
+
+    handleMore(e) {
+        e.preventDefault();
+        this.getUsers(false);
+        console.log('more users loaded');
+    }
     
-    getUsers() {
+    getUsers(replaceUsers = true) {
         this.setState({
             loading: true
         })
         axios('https://api.randomuser.me/?nat=US&results=5')
             .then(response => {
                 this.setState({
-                    users: response.data.results,
+                    users: replaceUsers ? response.data.results : [...this.state.users, ...response.data.results],
                     loading: false
                 })
             })
@@ -31,6 +47,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="submit" value="load users" />
+                </form>
+                <button onClick={this.handleMore}>More users</button>
                 {this.state.loading ? 
                     <Loading message="Not loaded yet" /> 
                     : this.state.users.map((user, index) => 
