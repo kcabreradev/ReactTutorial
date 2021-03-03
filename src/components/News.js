db793978b0b050c3198a87ecf1d4406941478448
kcabrera <kcabrera@tiempodevelopment.com>
2021-03-03 09:53:35 -0700

    fetch news and display them, added search input

--- src/components/News.js
+++ src/components/News.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+
 export const News = () => {
-    return null;
+    const [news, setNews] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('react');
+
+    const fetchNews = () => {
+        fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+            .then(result => result.json())
+            .then(data => setNews(data.hits))
+            .catch(error => console.log(error));
+    }
+
+    const handleChange = (e) => {
+        setSearchQuery(e.target.value)
+    }
+
+    useEffect(() => {
+        fetchNews();
+    }, [searchQuery]);
+
+    return (
+        <div>
+            <h2>News</h2>
+            <form>
+                <input type="text" value={searchQuery} onChange={handleChange} />
+                <button type="submit">Search</button>
+            </form>
+            {news.map((n, i) => (<p key={i}>{n.title}</p>))}
+        </div>
+    );
 }
