35c1974b02ac54188588076b61aa30c758c55264
kcabrera <kcabrera@tiempodevelopment.com>
2021-03-02 17:42:02 -0700

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+export function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand me-4">Course Project</Link>
+                <div className="me-auto">
+                    <ul className="navbar-nav flex-row">
+                        <li className="nav-item mx-3">
+                            <Link to="/users" className="nav-link" activeClassName="active">Users</Link>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <Link to="/counter" className="nav-link" activeClassName="active">Counter</Link>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <Link to="/test2" className="nav-link" activeClassName="active">test2</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
