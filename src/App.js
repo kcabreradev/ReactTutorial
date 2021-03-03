9728b476fa81e43b330e47e2cd49a91c59fdffae
kcabrera <kcabrera@tiempodevelopment.com>
2021-03-02 17:41:39 -0700

    added routing to app

--- src/App.js
+++ src/App.js
@@ -1,70 +1,27 @@
-import { Component } from "react";
-import axios from 'axios';
-import { Loading } from './Loading';
+import { Navbar } from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import { Users } from './components/Users';
+import { Counter } from './components/Counter';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        //state
-        this.state = {
-            users: [],
-            loading: false
-        }
-        //bind
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleMore = this.handleMore.bind(this)
-    }
-
-    componentDidMount() {
-        this.getUsers();
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        this.getUsers();
-        console.log('different users loaded');
-    }
-
-    handleMore(e) {
-        e.preventDefault();
-        this.getUsers(false);
-        console.log('more users loaded');
-    }
-    
-    getUsers(replaceUsers = true) {
-        this.setState({
-            loading: true
-        })
-        axios('https://api.randomuser.me/?nat=US&results=5')
-            .then(response => {
-                const {results} = response.data;
-                this.setState({
-                    users: replaceUsers ? results : [...this.state.users, ...results],
-                    loading: false
-                })
-            })
-    }
-
-    render() {
-        const {loading, users} = this.state;
-        return (
-            <div className="App">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="submit" value="load users" />
-                </form>
-                <button onClick={this.handleMore}>More users</button>
-                {loading ? 
-                    <Loading message="Not loaded yet" /> 
-                    : users.map((user, index) => 
-                    <div key={index}>
-                        <h3 style={{color: 'red'}}>{user.name.first} {user.name.last}</h3>
-                        <p>{user.email}</p>
-                        <hr />
-                    </div>
-                )}
+function App() {
+    return (
+        <Router>
+            <div className="container-fluid">
+                <Navbar></Navbar>
+                <hr />
+                <Switch>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/counter">
+                        <Counter />
+                    </Route>
+                    <Route path="/">
+                        <Redirect to="/users"></Redirect>
+                    </Route>
+                </Switch>
             </div>
-        );
-    }
+        </Router>
+    );
 }
-
 export default App;
